c9795a4db60689633f0eae0e4996533f23c21a5f
Your Nameyuliiazav <didiky1993@gmail.com>
2022-08-27 15:47:31 +0300

    added HW8 files

---
+++ js/script.js
@@ -0,0 +1,45 @@
+let store = {
+    name: "Cool Pets",
+    address: {
+        country: "Ukraine",
+        city: "Kyiv",
+        street: "Sunny",
+        houseNumber: 15
+    }
+};
+store.clients = {};
+
+store.clients = {
+    client1: {
+        firstName: "Yuliia",
+        secondName: "Zavistovska",
+        quantityOfPurchases: 15,
+        bonuses: 1500
+    },
+    client2: {
+        firstName: "Bohdan",
+        secondName: "Onatskyi",
+        quantityOfPurchases: 25,
+        bonuses: 2500
+    },
+    client3: {
+        firstName: "Olga",
+        secondName: "Kovalova",
+        quantityOfPurchases: 5,
+        bonuses: 500
+    },
+    client4: {
+        firstName: "Alina",
+        secondName: "Kholod",
+        quantityOfPurchases: 35,
+        bonuses: 3500
+    },
+    client5: {
+        firstName: "Yevhen",
+        secondName: "Vuzlov",
+        quantityOfPurchases: 18,
+        bonuses: 1800
+    }
+};
+
+
